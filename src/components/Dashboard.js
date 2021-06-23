4b4db03e8503688c855b6cc9b9284155d1b656f3
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2021-06-23 08:25:19 +0200

    Fetched trucks and rendered then on Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import classes from './Dashboard.module.css';
+
+const API_URL =
+  'https://truckoo-backend-aqkoiog6bq-ew.a.run.app/rest/v1/offers/active-offers';
 
 const Dashboard = () => {
+  const [trucks, setTrucks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(API_URL, { headers: { 'accept-language': 'en-US' } })
+      .then((response) => {
+        setTrucks(response.data);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className={classes.dashboard}>
+      <ul>
+        {trucks.map((truck) => (
+          <li key={truck.uid}>{truck.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
