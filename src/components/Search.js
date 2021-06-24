61359cb27470dc9a417dbb7f3ed95c0d8848dc9f
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2021-06-25 01:29:19 +0200

    Made search reactive

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,31 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { searchTrucks } from '../actions/trucksActions';
 
 const Search = () => {
-  const [searchString, setSearchString] = useState('');
   const dispatch = useDispatch();
 
   const handleInputChange = (e) => {
-    setSearchString(e.target.value);
-  };
-
-  const handleSearchSubmit = (e) => {
-    if (e.type === 'click' || e.key === 'Enter') {
-      dispatch(searchTrucks(searchString));
-      setSearchString('');
-    }
+    dispatch(searchTrucks(e.target.value));
   };
 
   return (
     <div>
-      <input
-        type='text'
-        onChange={handleInputChange}
-        onKeyPress={handleSearchSubmit}
-        value={searchString}
-      />
-      <button onClick={handleSearchSubmit}>Search</button>
+      <input type='text' onChange={handleInputChange} placeholder='Search' />
     </div>
   );
 };
