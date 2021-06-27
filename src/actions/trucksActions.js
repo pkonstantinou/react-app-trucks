1f185c98126b806f6e4486d553f2e316a5d07030
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2021-06-27 14:05:24 +0200

    Added error handling at truck fetching failure

--- src/actions/trucksActions.js
+++ src/actions/trucksActions.js
@@ -2,11 +2,15 @@ import axios from 'axios';
 import { allTrucksUrl } from '../api';
 
 export const fetchTrucks = () => async (dispatch) => {
-  const { data } = await axios.get(allTrucksUrl, {
-    headers: { 'accept-language': 'en-US' }
-  });
+  try {
+    const { data } = await axios.get(allTrucksUrl, {
+      headers: { 'accept-language': 'en-US' }
+    });
 
-  dispatch({ type: 'FETCH_TRUCKS', payload: { trucks: data } });
+    dispatch({ type: 'FETCH_TRUCKS', payload: { trucks: data } });
+  } catch (err) {
+    console.log(err, '\nFailed to fetch trucks!');
+  }
 };
 
 export const searchTrucks = (searchString) => async (dispatch, getState) => {
