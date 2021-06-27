2c3d4cfca56c839d0ca994105c70fe5dd43765b5
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2021-06-27 19:41:34 +0200

    Added Spinner component

---
+++ src/components/UI/Spinner.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import classes from './Spinner.module.css';
+
+const loadingContainerVariants = {
+  start: {
+    transition: {
+      staggerChildren: 0.1
+    }
+  },
+  end: {
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+};
+
+const loadingCircleVariants = {
+  start: {
+    y: '0%'
+  },
+  end: {
+    y: '100%'
+  }
+};
+
+const loadingCircleTransition = {
+  duration: 0.4,
+  yoyo: Infinity,
+  ease: 'easeInOut'
+};
+
+const Spinner = ({ className }) => {
+  return (
+    <motion.div
+      className={`${className} ${classes.spinner}`}
+      variants={loadingContainerVariants}
+      initial='start'
+      animate='end'
+    >
+      <motion.span
+        className={classes['spinner-circle']}
+        variants={loadingCircleVariants}
+        transition={loadingCircleTransition}
+      />
+      <motion.span
+        className={classes['spinner-circle']}
+        variants={loadingCircleVariants}
+        transition={loadingCircleTransition}
+      />
+      <motion.span
+        className={classes['spinner-circle']}
+        variants={loadingCircleVariants}
+        transition={loadingCircleTransition}
+      />
+    </motion.div>
+  );
+};
+
+export default Spinner;
